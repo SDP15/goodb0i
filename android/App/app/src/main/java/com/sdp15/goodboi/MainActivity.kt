e93b5ff9a3b5552cf7236043556144708641a305
Theo Pearson-Bray <tpb1908dev@gmail.com>
2019-01-23 10:46:35 +0000

    Add simple ViewModel for LoginFragment.

--- android/App/app/src/main/java/com/sdp15/goodboi/MainActivity.kt
+++ android/App/app/src/main/java/com/sdp15/goodboi/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
     }
 
     override fun onSupportNavigateUp(): Boolean = findNavController(R.id.nav_host_fragment).navigateUp()
