e00381c98fecf17c34e112cb0d8df4676cc6aa81
patriciam97 <patriciamilou97@gmail.com>
2019-02-01 17:24:56 +0000

    Added layout_shoppinglist.xml

--- android/App/app/src/main/java/com/sdp15/goodb0i/view/welcome/WelcomeViewModel.kt
+++ android/App/app/src/main/java/com/sdp15/goodb0i/view/welcome/WelcomeViewModel.kt
@@ -11,20 +11,13 @@ class WelcomeViewModel : BaseViewModel<WelcomeViewModel.WelcomeAction>(), Welcom
     }
 
     override fun prepareOrder() {
-<<<<<<< HEAD
-        actions.postValue(WelcomeAction.Navigate(R.id.search_fragment))
-    }
-
-    override fun startShopping() {
-        actions.postValue(WelcomeAction.Navigate(R.id.pin_fragment))
-=======
         transitions.postValue(R.id.search_fragment)
     }
 
     override fun startShopping() {
         transitions.postValue(R.id.pin_fragment)
->>>>>>> b0a0dfe4effe54d8e76fedc16942ef9a198bc96e
     }
+
 
     sealed class WelcomeAction {
 
