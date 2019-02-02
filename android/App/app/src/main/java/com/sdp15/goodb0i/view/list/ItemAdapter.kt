9c90cc420c6d7f27c7a8317e60696c7e4f3302b4
Theo Pearson-Bray <tpb1908dev@gmail.com>
2019-02-02 13:40:26 +0000

    Move list view button container expansion state out of ViewHolders to fix behaviour when recycled.

--- android/App/app/src/main/java/com/sdp15/goodb0i/view/list/ItemAdapter.kt
+++ android/App/app/src/main/java/com/sdp15/goodb0i/view/list/ItemAdapter.kt
@@ -3,34 +3,36 @@ package com.sdp15.goodb0i.view.list
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import androidx.appcompat.widget.AppCompatImageButton
-import androidx.appcompat.widget.AppCompatTextView
 import androidx.recyclerview.widget.RecyclerView
 import com.sdp15.goodb0i.R
 import com.sdp15.goodb0i.collapse
 import com.sdp15.goodb0i.data.store.Item
 import com.sdp15.goodb0i.expand
-import timber.log.Timber
+import kotlinx.android.synthetic.main.list_item.view.*
 
-class ItemAdapter(val onIncrement: (Item) -> Unit, val onDecrement: (Item) -> Unit) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+class ItemAdapter(val onIncrement: (Item) -> Unit, val onDecrement: (Item) -> Unit) :
+    RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
     private var items: MutableList<CartItem> = mutableListOf()
+    private var expanded: MutableList<Boolean> = mutableListOf()
 
     fun itemsChanged(diff: ListDiff<CartItem>) {
         when (diff) {
             is ListDiff.All -> {
                 items = diff.items.toMutableList()
+                expanded = items.map { false }.toMutableList()
                 notifyDataSetChanged()
             }
             is ListDiff.Add -> {
                 val i = diff.items.indexOf(diff.item)
                 items = diff.items.toMutableList()
+                expanded.add(i, false)
                 notifyItemInserted(i)
             }
             is ListDiff.Remove -> {
                 val i = items.indexOf(diff.item)
                 items = diff.items.toMutableList()
+                expanded.removeAt(i)
                 notifyItemRemoved(i)
             }
             is ListDiff.Update -> {
@@ -41,20 +43,10 @@ class ItemAdapter(val onIncrement: (Item) -> Unit, val onDecrement: (Item) -> Un
     }
 
     sealed class ListDiff<T>(val items: List<T>) {
-        class All<T>(items: List<T>): ListDiff<T>(items)
+        class All<T>(items: List<T>) : ListDiff<T>(items)
         class Add<T>(items: List<T>, val item: T) : ListDiff<T>(items)
-        class Remove<T>(items: List<T>, val item: T): ListDiff<T>(items)
-        class Update<T>(items: List<T>, val item: T): ListDiff<T>(items)
-    }
-
-    fun itemCountChanged(item: CartItem) {
-        val i = items.indexOfFirst { it.item.id == item.item.id }
-        if (i != -1) {
-            items[i] = item
-            notifyItemChanged(i)
-        } else {
-            Timber.e("Item $item should have been in adapter, but wasn't")
-        }
+        class Remove<T>(items: List<T>, val item: T) : ListDiff<T>(items)
+        class Update<T>(items: List<T>, val item: T) : ListDiff<T>(items)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -64,39 +56,26 @@ class ItemAdapter(val onIncrement: (Item) -> Unit, val onDecrement: (Item) -> Un
     override fun getItemCount(): Int = items.size
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        holder.item = items[position].item
-    }
-
-    inner class ItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-        private val buttons = view.findViewById<LinearLayout>(R.id.item_button_container)
-        private val name = view.findViewById<AppCompatTextView>(R.id.text_item_name)
-        private val price = view.findViewById<AppCompatTextView>(R.id.text_item_price)
-        private val quantity = view.findViewById<AppCompatTextView>(R.id.text_item_quantity)
-        private val positive = view.findViewById<AppCompatImageButton>(R.id.button_positive)
-        private val negative = view.findViewById<AppCompatImageButton>(R.id.button_negative)
-        
-        var item: Item? = null
-            set(value) {
-                field = value
-                value?.let { item ->
-                    name.text = item.name
-                    price.text = item.price.toString()
-                    quantity.text = item.contentsQuantity.toString()
-                    positive.setOnClickListener { onIncrement(item) }
-                    negative.setOnClickListener { onDecrement(item) }
-                }
-            }
-
-        init {
-            view.setOnClickListener {
-                if (buttons.visibility == View.GONE) {
-                    expand(buttons)
+        holder.view.apply {
+            item_button_container.visibility = if (expanded[position]) View.VISIBLE else View.GONE
+            setOnClickListener {
+                if (expanded[position]) {
+                    collapse(item_button_container)
                 } else {
-                    collapse(buttons)
+                    expand(item_button_container)
                 }
+                expanded[position] = !expanded[position]
             }
-        }
+            val item = items[position].item
+            text_item_name.text = item.name
+            text_item_quantity.text = item.unitQuantity
+            text_item_price.text = item.price.toString()
+            button_positive.setOnClickListener { onIncrement(item) }
+            button_negative.setOnClickListener { onDecrement(item) }
 
+        }
     }
+
+    inner class ItemViewHolder(val view: View) : RecyclerView.ViewHolder(view)
 
 }
