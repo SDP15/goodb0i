316d0aa4008ab45309d86447500054a4d2bc6dd5
Jakub Szewczyk <kubasz51@gmail.com>
2019-02-03 17:33:46 +0000

    Steering calibration

--- ev3/g0b-rootev3/main.cpp
+++ ev3/g0b-rootev3/main.cpp
@@ -1,13 +1,16 @@
+#include <array>
 #include <cassert>
 #include <chrono>
 #include <cstdint>
+#include <fstream>
 #include <iostream>
 #include <memory>
+#include <signal.h>
 #include <thread>
 #include <unistd.h>
 
-#include "ev3dev.h"
 #include "ev3dev.cpp"
+#include "ev3dev.h"
 #include "util.hpp"
 
 using namespace std;
@@ -16,7 +19,21 @@ using namespace ev3dev;
 unique_ptr<evutil::Drive> leftDrive, rightDrive, steerDrive;
 unique_ptr<ev3dev::color_sensor> leftColor, rightColor;
 unique_ptr<ev3dev::ultrasonic_sensor> sonar;
-unique_ptr<ev3dev::touch_sensor> wheelCalibrationSensor; 
+unique_ptr<ev3dev::touch_sensor> wheelCalibrationSensor;
+
+ofstream sensorLog;
+
+/// Helper function to log messages with timestamps.
+/// Usage: log() << "My log message" << endl;
+ostream &log() {
+  auto now{time(nullptr)};
+  array<char, 100> timeStr;
+  size_t timeStrLen{strftime(timeStr.data(), timeStr.size(), "%T ",
+                             localtime(&now))}; // DevSkim: ignore DS154189
+  sensorLog.write(timeStr.data(), timeStrLen);
+  // return sensorLog;
+  return cerr;
+}
 
 void connectEv3Devices() {
   bool failed{false};
@@ -26,23 +43,89 @@ void connectEv3Devices() {
   rightDrive->setReversed();
   steerDrive = make_unique<evutil::Drive>(OUTPUT_C, failed);
 
-  leftColor = evutil::createConnectedDevice<ev3dev::color_sensor>(INPUT_1, failed);
-  rightColor = evutil::createConnectedDevice<ev3dev::color_sensor>(INPUT_2, failed);
-  
-  sonar = evutil::createConnectedDevice<ev3dev::ultrasonic_sensor>(INPUT_3, failed);
-  wheelCalibrationSensor = evutil::createConnectedDevice<ev3dev::touch_sensor>(INPUT_4, failed);
+  leftColor =
+      evutil::createConnectedDevice<ev3dev::color_sensor>(INPUT_1, failed);
+  rightColor =
+      evutil::createConnectedDevice<ev3dev::color_sensor>(INPUT_2, failed);
+
+  sonar =
+      evutil::createConnectedDevice<ev3dev::ultrasonic_sensor>(INPUT_3, failed);
+  wheelCalibrationSensor =
+      evutil::createConnectedDevice<ev3dev::touch_sensor>(INPUT_4, failed);
 
   if (failed) {
     throw runtime_error("Error initializing EV3 connections.");
   }
 }
 
+void openLogFile() {
+  auto now{time(nullptr)};
+  array<char, 100> logFilename;
+  size_t logNameLen{strftime(logFilename.data(), logFilename.size(),
+                             "zlog-g0b-%F-%H-%M-%S.log",
+                             localtime(&now))}; // DevSkim: ignore DS154189
+  string logName{logFilename.data(), logNameLen};
+  sensorLog = ofstream(logName, ios_base::out | ios_base::app);
+  if (!sensorLog.good()) {
+    throw runtime_error("Could not open logfile.");
+  }
+}
+
 class Robot {
 private:
-
 public:
-  Robot() {
-    
+  Robot() { calibrateSteering(); }
+
+  void calibrateSteering() {
+    cout << "Calibrating steering..." << endl;
+    log() << "** STEERING CALIBRATION **" << endl;
+    log() << "* Phase 1: find left *" << endl;
+    log() << "start pos = " << steerDrive->getPosition() << endl;
+
+    // Go to extreme left
+    steerDrive->overrideStopAction(motor::stop_action_coast);
+    steerDrive->runForever(-10); // run at low speed not to break anything
+    this_thread::sleep_for(chrono::milliseconds{1200});
+    steerDrive->stop();
+    log() << "end pos = " << steerDrive->getPosition() << endl;
+    steerDrive->setPosition(0);
+
+    // Make sure sensor mode is set for short reaction times.
+    wheelCalibrationSensor->is_pressed(true);
+
+    log() << "* Phase 2: find minmax *" << endl;
+    // Get min and max touch positions
+    int minTouchPos, maxTouchPos;
+    {
+      steerDrive->stop();
+      this_thread::yield();
+      // Keep this number high, otherwise the motor turns very poorly and leads
+      // to very inaccurate results
+      steerDrive->runForever(40);
+      // wait for it to reach the touch sensor
+      while (!wheelCalibrationSensor->is_pressed(false)) {
+        this_thread::yield();
+      }
+      minTouchPos = steerDrive->getPosition();
+      // and then for it to not be pressed anymore
+      while (wheelCalibrationSensor->is_pressed(false)) {
+        this_thread::yield();
+      }
+      maxTouchPos = steerDrive->getDegrees();
+    }
+    steerDrive->stop();
+    int calibratedPos{(minTouchPos + maxTouchPos) / 2};
+    log() << "min pos = " << minTouchPos << endl;
+    log() << "max pos = " << maxTouchPos << endl;
+    log() << "cal pos = " << calibratedPos << endl;
+    // Set zero point to middle.
+    steerDrive->setPosition(steerDrive->getPosition() - calibratedPos);
+    steerDrive->resetStopAction();
+    // Center wheels
+    steerDrive->runToPosition(0);
+    steerDrive->waitUntilIdle();
+    log() << "** Wheels calibrated **" << endl;
+    cout << "Steering calibrated" << endl;
   }
 
   void process() {
@@ -67,24 +150,52 @@ public:
   }
 };
 
+/// Emergency motor disablement
+void disableMotors() {
+  array<evutil::Drive *, 3> motors{
+      {leftDrive.get(), rightDrive.get(), steerDrive.get()}};
+  for (auto &drive : motors) {
+    if (drive && drive->getMotor() && drive->getMotor()->connected()) {
+      drive->overrideStopAction(motor::stop_action_coast);
+      drive->stop();
+    }
+  }
+}
+
+volatile bool sigintTerminate{false};
+
+void sigintHandler(int sig) {
+  if (sig == SIGINT) {
+    sigintTerminate = true;
+  }
+}
+
 int main() {
   try {
+    if (signal(SIGINT, &sigintHandler) == SIG_ERR) {
+      cerr << "Could not register SIGINT handler, continuing..." << endl;
+    }
     connectEv3Devices();
+    openLogFile();
 
     Robot robot{};
-    while (!button::back.pressed()) {
+    while (!(sigintTerminate || button::back.pressed())) {
       robot.process();
-      this_thread::sleep_for(chrono::milliseconds(90));
+      this_thread::yield();
     }
+    disableMotors();
     return 0;
   } catch (const exception &err) {
     cerr << "[TERMINATE] Runtime error: " << err.what() << endl;
+    disableMotors();
     return 1;
   } catch (exception *err) {
     cerr << "[TERMINATE] Runtime error: " << err->what() << endl;
+    disableMotors();
     return 1;
   } catch (...) {
     cerr << "[TERMINATE] Unknown error occured" << endl;
+    disableMotors();
     return 2;
   }
 }
--- ev3/util/util.hpp
+++ ev3/util/util.hpp
@@ -22,19 +22,35 @@ private:
   std::unique_ptr<ev3dev::motor> mtr;
   bool reversed;
   int maxSpeed;
+  int countsPerDegree;
 
 public:
   inline Drive(ev3dev::address_type motor_addr, bool &failFlag) {
     mtr = createConnectedDevice<ev3dev::motor>(motor_addr, failFlag);
     reversed = false;
     maxSpeed = mtr->max_speed();
+    countsPerDegree = mtr->count_per_rot() / 360;
     std::cerr << "Motor " << motor_addr << " max speed: " << maxSpeed
-              << std::endl;
+              << " P/deg: " << countsPerDegree << std::endl;
     mtr->set_stop_action(ev3dev::motor::stop_action_hold);
     mtr->stop();
   }
 
+  /// Gets the encoder's position, adjusted for the reversed field.
+  inline int getPosition() {
+    int p{mtr->position()};
+    return reversed ? -p : p;
+  }
+
+  /// Changes the zero-point of the encoder for the current position to be pos.
+  inline void setPosition(int pos) { mtr->set_position(reversed ? -pos : pos); }
+
+  // converts encoder position to degrees
+  inline int getDegrees() { return getPosition() / countsPerDegree; }
+
   inline void setReversed(bool rev = true) { reversed = rev; }
+
+  inline bool isReversed() { return reversed; }
 
   inline ev3dev::motor *getMotor() { return mtr.get(); }
 
@@ -45,6 +61,33 @@ public:
     mtr->set_speed_sp(reversed ? -sp : sp);
     mtr->run_forever();
   }
+
+  inline void overrideStopAction(const char *action) {
+    mtr->set_stop_action(action);
+  }
+
+  inline void resetStopAction() {
+    mtr->set_stop_action(ev3dev::motor::stop_action_hold);
+  }
+
+  inline void runTimed(int time_ms, int speedPercent = 100) {
+    int sp{speedPercent * maxSpeed / 100};
+    mtr->set_speed_sp(reversed ? -sp : sp);
+    mtr->run_timed();
+  }
+
+  inline void waitUntilIdle() {
+    while (mtr->state().count(ev3dev::motor::state_running)) {
+      std::this_thread::sleep_for(std::chrono::milliseconds{50});
+    }
+  }
+
+  inline void runToPosition(int pos) {
+    mtr->set_position_sp(pos);
+    mtr->run_to_abs_pos();
+  }
+
+  inline void runToDegree(int deg) { runToPosition(deg * countsPerDegree); }
 };
 
 } // namespace evutil
