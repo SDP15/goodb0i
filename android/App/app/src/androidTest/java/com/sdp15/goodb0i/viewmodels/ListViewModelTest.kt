618f49d36713d74df29a538130c6bb7c67241de2
Theo Pearson-Bray <tpb1908dev@gmail.com>
2019-02-04 15:15:10 +0000

    Add tests for ListViewModel.

--- android/App/app/src/androidTest/java/com/sdp15/goodb0i/viewmodels/ListViewModelTest.kt
+++ android/App/app/src/androidTest/java/com/sdp15/goodb0i/viewmodels/ListViewModelTest.kt
@@ -2,26 +2,21 @@ package com.sdp15.goodb0i.viewmodels
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
-import androidx.test.runner.AndroidJUnit4
-import androidx.test.runner.AndroidJUnitRunner
 import com.nhaarman.mockitokotlin2.*
 import com.sdp15.goodb0i.data.store.Item
-import com.sdp15.goodb0i.data.store.ItemLoader
 import com.sdp15.goodb0i.data.store.TestDataItemLoader
 import com.sdp15.goodb0i.view.ListDiff
 import com.sdp15.goodb0i.view.list.ListViewModel
 import com.sdp15.goodb0i.view.list.TrolleyItem
+import io.mockk.CapturingSlot
 import io.mockk.mockk
 import io.mockk.slot
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 import org.koin.test.KoinTest
 import org.koin.test.declare
-import org.koin.test.declareMock
-import org.mockito.Mockito.`when`
 
 class ListViewModelTest : KoinTest {
 
@@ -29,50 +24,89 @@ class ListViewModelTest : KoinTest {
     var instantTaskExecutorRule = InstantTaskExecutorRule()
 
     private lateinit var vm: ListViewModel
-    private lateinit var searchCaptor: KArgumentCaptor<List<TrolleyItem>>
-    private lateinit var listCaptor: KArgumentCaptor<ListDiff<TrolleyItem>>
-    private lateinit var priceCaptor: KArgumentCaptor<Double>
-    private lateinit var searchObserver: Observer<List<TrolleyItem>>
+    private lateinit var item: Item
     private lateinit var listObserver: Observer<ListDiff<TrolleyItem>>
-
+    private lateinit var listSlot: CapturingSlot<ListDiff<TrolleyItem>>
     @Before
     fun setUp() {
 //        declareMock<ItemLoader> {
 //            `when`(this::search).thenReturn()
 //        }
         //declareMock<ItemLoader>()
-        searchCaptor = argumentCaptor()
-        listCaptor = argumentCaptor()
-        priceCaptor = argumentCaptor()
-        searchObserver = mock()
-        listObserver = mock()
+        listObserver = mockk(relaxed = true)
+        item = mockk(relaxed = true)
+        listSlot = io.mockk.slot()
         declare { TestDataItemLoader }
         vm = ListViewModel()
         vm.bind()
+        vm.list.observeForever(listObserver)
     }
 
     @Test
     fun testEmptySearch() {
-
+        val searchObserver: Observer<List<TrolleyItem>> = mockk(relaxed = true)
+        val slot = slot<List<TrolleyItem>>()
         vm.searchResults.observeForever(searchObserver)
         vm.onQueryChange("", "")
-        verify(searchObserver, times(1)).onChanged(searchCaptor.capture())
-        Assert.assertEquals("Search results should be empty", 0, searchCaptor.firstValue.size)
+        io.mockk.verify(exactly = 1) {
+            searchObserver.onChanged(capture(slot))
+        }
+        Assert.assertEquals("Search results should be empty", 0, slot.captured.size)
     }
 
     @Test
     fun testIncrementItem() {
-        val item: Item = mockk(relaxed = true)
-        val observer: Observer<ListDiff<TrolleyItem>> = mockk(relaxed = true)
-        val slot = slot<ListDiff<TrolleyItem>>()
-        vm.list.observeForever(observer)
+
         vm.incrementItem(item)
         io.mockk.verify(exactly = 1) {
-            observer.onChanged(capture(slot))
+            listObserver.onChanged(capture(listSlot))
         }
-        Assert.assertTrue("ListDiff should be add", slot.captured is ListDiff.Add)
-        Assert.assertEquals("Same item should be returned", item, (slot.captured as ListDiff.Add).item.item)
-        Assert.assertEquals("Count should be 1", 1, (slot.captured as ListDiff.Add).item.count)
+        Assert.assertTrue("ListDiff should be add", listSlot.captured is ListDiff.Add)
+        Assert.assertEquals("Same item should be returned", item, (listSlot.captured as ListDiff.Add).item.item)
+        Assert.assertEquals("Count should be 1", 1, (listSlot.captured as ListDiff.Add).item.count)
     }
 
+    @Test
+    fun testIncrementItemTwice() {
+        vm.incrementItem(item)
+        vm.incrementItem(item)
+        io.mockk.verify(exactly = 2) {
+            listObserver.onChanged(any())
+            listObserver.onChanged(capture(listSlot))
+        }
+        Assert.assertTrue("ListDiff should be update", listSlot.captured is ListDiff.Update)
+        Assert.assertEquals("Same item should be returned", item, (listSlot.captured as ListDiff.Update).item.item)
+        Assert.assertEquals("Count should be 2", 2, (listSlot.captured as ListDiff.Update).item.count)
+    }
+
+    @Test
+    fun testDecrementOnce() {
+        vm.incrementItem(item)
+        vm.incrementItem(item)
+        vm.decrementItem(item)
+        io.mockk.verify(exactly = 3) {
+            listObserver.onChanged(any())
+            listObserver.onChanged(any())
+            listObserver.onChanged(capture(listSlot))
+        }
+        Assert.assertTrue("Diff should be update", listSlot.captured is ListDiff.Update)
+        Assert.assertEquals("Item should be the same", item, (listSlot.captured as ListDiff.Update).item.item)
+        Assert.assertEquals("Quantity should be 1", 1, (listSlot.captured as ListDiff.Update).item.count)
+    }
+
+    @Test
+    fun testDecrementToRemoval() {
+        vm.incrementItem(item)
+        vm.decrementItem(item)
+        io.mockk.verify(exactly = 2) {
+            listObserver.onChanged(any())
+            listObserver.onChanged(capture(listSlot))
+        }
+        Assert.assertTrue("Diff should be remove", listSlot.captured is ListDiff.Remove)
+        Assert.assertEquals("Item should be the same", item, (listSlot.captured as ListDiff.Remove).item.item)
+        Assert.assertFalse(
+            "List should not contain item",
+            listSlot.captured.items.contains((listSlot.captured as ListDiff.Remove).item)
+        )
+    }
 }
