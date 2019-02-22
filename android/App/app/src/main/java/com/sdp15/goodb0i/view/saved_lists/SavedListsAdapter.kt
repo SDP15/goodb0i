a4f70ddf41710ffee56c91e4cc0545a40a70c422
Theo Pearson-Bray <tpb1908dev@gmail.com>
2019-02-22 19:07:02 +0000

    Add preview text to SavedListsAdapter items.

--- android/App/app/src/main/java/com/sdp15/goodb0i/view/saved_lists/SavedListsAdapter.kt
+++ android/App/app/src/main/java/com/sdp15/goodb0i/view/saved_lists/SavedListsAdapter.kt
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.sdp15.goodb0i.R
 import com.sdp15.goodb0i.data.store.lists.ShoppingList
 import kotlinx.android.synthetic.main.list_order.view.*
+import kotlin.math.min
 
 class SavedListsAdapter(val onClick: (ShoppingList) -> Unit) : RecyclerView.Adapter<SavedListsAdapter.SavedListViewHolder>() {
 
@@ -24,11 +25,17 @@ class SavedListsAdapter(val onClick: (ShoppingList) -> Unit) : RecyclerView.Adap
     override fun getItemCount(): Int = lists.size
 
     override fun onBindViewHolder(holder: SavedListViewHolder, position: Int) {
-        holder.itemView.text_list_code.text = lists[position].code.toString()
-        holder.itemView.text_list_price.text = holder.itemView.context.getString(R.string.label_total_price,
-            lists[position].products.sumByDouble { it.quantity * it.product.price })
-        holder.itemView.setOnClickListener {
-            onClick(lists[position])
+        val sl = lists[position]
+        holder.itemView.apply {
+            text_list_code.text = sl.code.toString()
+            text_list_price.text = context.getString(R.string.label_total_price,
+                sl.products.sumByDouble { it.quantity * it.product.price })
+            text_list_preview.text = sl.products.take(min(3, sl.products.size)).joinToString(separator = "\n") { item ->
+                context.getString(R.string.label_list_item_info, item.quantity, item.product.name)
+            }
+            setOnClickListener {
+                onClick(sl)
+            }
         }
     }
 
